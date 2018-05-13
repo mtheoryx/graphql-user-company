c0b0b692f303d1dc877753830e07c84a1d8f8aa5
David Poindexter <drpoindexter@gmail.com>
2018-05-13 17:52:31 -0400

    Wire up fetching a single company, and all companies in root resolver

--- schema/schema.js
+++ schema/schema.js
@@ -68,6 +68,23 @@ const RootQuery = new GraphQLObjectType({
           .get(`http://localhost:3000/users/`)
           .then(response => response.data);
       }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(response => response.data);
+      }
+    },
+    getAllCompanies: {
+      type: new GraphQLList(CompanyType),
+      resolve() {
+        return axios
+          .get(`http://localhost:3000/companies/`)
+          .then(response => response.data);
+      }
     }
   }
 });
