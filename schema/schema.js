365fbfc37a99f466e4cde5ccc2702001371f1e9b
David Poindexter <drpoindexter@gmail.com>
2018-05-13 13:26:00 -0400

    Add basic schema and pseudocode object-relationship comments

---
+++ schema/schema.js
@@ -0,0 +1,24 @@
+// Users
+/** 
+ * id: Integer
+ * firstName: String
+ * company_id: Integer
+ * position_id: Integer
+ * users: Array<User>
+ * 
+ */
+
+// Companies
+/**
+ * id: Integer
+ * name: String
+ * description: String
+ * 
+ */
+
+// Positions
+/**
+ * id: Integer
+ * name: String
+ * description: String
+ */
